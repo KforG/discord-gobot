703b64ebfc916c70571fa3ec8f5a60b133cbff71
KforG <34801641+KforG@users.noreply.github.com>
2021-12-29 17:41:35 +0100

    delete unused function

--- util/util.go
+++ util/util.go
@@ -8,19 +8,7 @@ import (
 
 	"github.com/KforG/discord-price-bot/config"
 	"github.com/KforG/discord-price-bot/logging"
-	"github.com/bwmarrin/discordgo"
 )
-
-func StartDiscordSession(token string) interface{} {
-	//Create Discord session
-	dg, err := discordgo.New(config.BotPrefix + config.Token)
-	if err != nil {
-		logging.Errorf("error creating new Discord sesssion,", err)
-	}
-	logging.Info("Successfully created new discord session")
-
-	return dg
-}
 
 func GetJson(url string, target interface{}) error {
 	//Fetch data from api
